4fca0f2580ef19fbcb855f93c05f9c29ec86b6d8
migorithm <saka90030@gmail.com>
2021-06-28 10:17:05 +0900

    test

---
+++ com/migorithm/Algorithm/Points.java
@@ -0,0 +1,9 @@
+package com.migorithm.Algorithm;
+
+public class Points {
+    public static void main(String[] args) {
+        Point p1 = new Point(5,4);
+        Point p2 = new Point(8,3);
+        System.out.println(p1.distance(p2));
+    }
+}
