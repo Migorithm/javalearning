89fd3b6ce13c37c5f459769532b3f8bc6c277f53
Migorithm <saka90030@gmail.com>
2021-06-30 00:15:40 +0900

    multithreading

--- com/migorithm/Thread_handling/Main.java
+++ com/migorithm/Thread_handling/Main.java
@@ -1,7 +1,7 @@
 package com.migorithm.Thread_handling;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 /*  multithreading = Process of executing multiple threads simultaneously helps maximum utilization of CPU.
                     Threads are independent, they don't affect the execution of other threads. An exception in one thread will not
                     interrupt other threads. It's useful for serving multiple clients, multiplayer games or other mutually independent taks.s
@@ -12,10 +12,15 @@ public class Main {
         //Second way: you create class that inherit 'runnable' interface, passing that instance to the constructor of Thread class.
         // People tend to use runnable interface more.
         MyRunnable runnable1 = new MyRunnable();
+
         Thread thread2 = new Thread(runnable1); //within the parenthesis of thread, just place runnable instance. this is a way of creaing a thread.
+        Thread thread3 = new Thread(runnable1);
 
         thread1.start();   //they are both running at the same time!
+        thread1.join(); //this will make the specified thread to be finished so, if you want thread2 to start AFTER threat 1, you can do this. Plus, as it gets the thread to wait, it throw exception.
         thread2.start();
+        thread3.start();
+
 
 
     /* one of the great things about multi-threading is that if one of these threads encounters an exception and gets interrupted the other threat
