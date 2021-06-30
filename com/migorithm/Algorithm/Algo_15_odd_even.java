d7512d9810c3ecd5827217cb9eb5c83f83c2adaf
migorithm <saka90030@gmail.com>
2021-06-30 09:56:14 +0900

    gg

--- com/migorithm/Algorithm/Algo_15_odd_even.java
+++ com/migorithm/Algorithm/Algo_15_odd_even.java
@@ -1,4 +1,17 @@
 package com.migorithm.Algorithm;
-
+//https://programmers.co.kr/learn/courses/30/lessons/12937
 public class Algo_15_odd_even {
+    public static void main(String[] args) {
+        Algo_15_odd_even A = new Algo_15_odd_even();
+        System.out.println(A.solution(5));
+
+    }
+
+    public String solution(int num) {
+        if (num%2 ==1){
+            return "Odd";
+        }
+        else {return "Even";}
+
+    }
 }
