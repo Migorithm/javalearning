efbd9ebf9ee22dcd986edf09ab26fe800a3449cd
migorithm <saka90030@gmail.com>
2021-06-30 19:44:12 +0900

    byte_file

--- com/migorithm/PlayData/FileHandling/Byte_file.java
+++ com/migorithm/PlayData/FileHandling/Byte_file.java
@@ -2,7 +2,7 @@ package com.migorithm.PlayData.FileHandling;
 
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
+
 
 public class Byte_file {
 
@@ -14,9 +14,18 @@ public class Byte_file {
 
     public static void byte_read(File f) throws IOException{
         FileInputStream byte_input = new FileInputStream(f);
+
+
         int ch = 0;
-        while((ch = byte_input.read()) != -1){
-            System.out.printf("%c",ch);
+//        while((ch = byte_input.read()) != -1){
+//            System.out.printf("%c",ch);
+//            System.out.println();
+//        }
+        while(true){
+            System.out.printf("%c",byte_input.read());
+            if (byte_input.available() ==0){
+                break;
+            }
         }
     }
 
---
+++ com/migorithm/PlayData/FileHandling/Object_file.java
@@ -0,0 +1,4 @@
+package com.migorithm.PlayData.FileHandling;
+
+public class Object_file {
+}
