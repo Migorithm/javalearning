3ea1920a7ca04644146d9ad72c88ffaaf517e464
migorithm <saka90030@gmail.com>
2021-07-09 16:24:36 +0900

    gdgd

---
+++ com/migorithm/Algorithm/Algo_23_numk.java
@@ -0,0 +1,28 @@
+package com.migorithm.Algorithm;
+//https://programmers.co.kr/learn/courses/30/lessons/42748?language=java
+
+import java.util.Arrays;
+
+public class Algo_23_numk {
+    public static void main(String[] args) {
+        Algo_23_numk A = new Algo_23_numk();
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        A.solution(array,commands);
+    }
+
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0 ; i < commands.length ; i++){
+            int[] copy = Arrays.copyOfRange(array,commands[i][0],commands[i][1]);
+            answer[i] = copy[commands[i][2]];
+        }
+
+
+
+
+
+        return answer;
+    }
+}
